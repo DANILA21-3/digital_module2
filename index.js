9b4cffe0f1b20b2e7ac5b0aee587c458f738a57d
DANILA21-3 <cherdanila184@gmail.com>
2025-04-03 17:46:44 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function loadData() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "data.json", true);
+    request.onload = function () {
+        if (request.status === 200) {
+            try {
+                var data = JSON.parse(request.responseText);
+                console.log("Загрузка выполнена успешно:", data);
+                simulateFalseError();
+            }
+            catch (error) {
+                console.error("Парсинг не удался:", error);
+            }
+        }
+        else {
+            console.error("Ошибка загрузки данных:", request.statusText);
+        }
+    };
+    request.onerror = function () {
+        console.error("Ошибка сети");
+    };
+    request.send();
+}
+loadData();
+function simulateFalseError() {
+    try {
+        throw new Error("Это ложная ошибка");
+    }
+    catch (error) {
+        console.error("Обработка ложной ошибки:", error.message);
+    }
+}
---
+++ index.ts
@@ -0,0 +1,36 @@
+function loadData() {
+    const request = new XMLHttpRequest();
+    request.open("GET", "data.json", true);
+    
+    request.onload = () => {
+        if (request.status === 200) {
+            try {
+                const data = JSON.parse(request.responseText);
+                console.log("Загрузка выполнена успешно:", data);
+                
+                simulateFalseError();
+
+            } catch (error) {
+                console.error("Парсинг не удался:", error);              
+            }
+        } else {
+            console.error("Ошибка загрузки данных:", request.statusText);     
+        }
+    };
+
+    request.onerror = () => {
+        console.error("Ошибка сети");
+    };
+
+    request.send();
+}
+
+loadData();
+
+function simulateFalseError() {
+    try {
+        throw new Error("Это ложная ошибка");
+    } catch (error) {
+        console.error("Обработка ложной ошибки:", error.message);
+    }
+}
